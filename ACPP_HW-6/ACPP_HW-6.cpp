528317a6916f9b9ac6aeffdd216b347fca57ec2a
Macilnor <macilnor@mail.ru>
2021-09-29 10:50:57 +0300

    Задание 1

--- ACPP_HW-6/ACPP_HW-6.cpp
+++ ACPP_HW-6/ACPP_HW-6.cpp
@@ -1,6 +1,27 @@
 ﻿#include <iostream>
+#include <chrono>
+#include <thread>
+#include "pcout.h"
+
+using namespace std;
+
+std::mutex pcout::_mutexPrint{};
+
+void printId() {
+    pcout{} << "Thread " << this_thread::get_id() << " started." << endl;
+    this_thread::sleep_for(1s);
+    pcout{} << "Thread " << this_thread::get_id() << " stoped." << endl;
+}
+
 
 int main()
 {
+    thread th1(printId);
+    thread th2(printId);
+    thread th3(printId);
+    th1.join();
+    th2.join();
+    th3.join();
+
 
 }
