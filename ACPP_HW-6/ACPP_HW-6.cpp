746d1c4423f161b6c961a2e54cfbbc2f8a680fbb
Macilnor <macilnor@mail.ru>
2021-09-29 14:58:46 +0300

    Задание 2

--- ACPP_HW-6/ACPP_HW-6.cpp
+++ ACPP_HW-6/ACPP_HW-6.cpp
@@ -1,18 +1,21 @@
 ﻿#include <iostream>
 #include <chrono>
 #include <thread>
+#include <vector>
+#include <stdlib.h>
 #include "pcout.h"
+#include "findPrime.h"
 
 using namespace std;
 
 std::mutex pcout::_mutexPrint{};
 
-void printId() {
+void printId()
+{
     pcout{} << "Thread " << this_thread::get_id() << " started." << endl;
     this_thread::sleep_for(1s);
     pcout{} << "Thread " << this_thread::get_id() << " stoped." << endl;
 }
-
 
 int main()
 {
@@ -23,5 +26,10 @@ int main()
     th2.join();
     th3.join();
 
-
+    size_t n;
+    system("cls");
+    std::cout << "Enter index of prime number: ";
+    std::cin >> n;
+    thread t_prime(findPrime, n);
+    t_prime.join();
 }
